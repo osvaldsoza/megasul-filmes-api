0db06ff4d46e697b9b5a68d38282bc21384f54ed
Osvaldo Ferreira <osvaldoferreira@Osvaldos-MacBook-Pro.local>
2019-10-16 18:56:46 -0300

    atualizacao

--- src/main/java/br/com/osvaldsoza/filmesapi/resource/FilmeResource.java
+++ src/main/java/br/com/osvaldsoza/filmesapi/resource/FilmeResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/filmes")
 @CrossOrigin(maxAge = 10, origins = {"*"})
 public class FilmeResource {
 
